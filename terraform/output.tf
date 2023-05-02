cebf487667d28a3f4192e2fa0ac039f59df70d1e
Ashutoshh <asashuthegreat4@gmail.com>
2023-05-02 12:23:54 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,11 @@
 output "my-vm-ip" {
   value = aws_instance.ashu-vm1[0].public_ip
+  
+}
+output "vm2-ip" {
+  value = aws_instance.ashu-vm1[1].public_ip
+}
+
+output "list-ip" {
+  value = [ for ip in aws_instance.ashu-vm1 : ip.public_ip ]
 }
